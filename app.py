f1679b96997eef012609331816611401a1f300e9
asimonia <alex.simonian@gmail.com>
2016-04-15 01:05:29 -0500

    Add datetime to post

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
+from database import Database
 from models.post import Post
 
+Database.initialize()
+
+post = Post(blog_id='123',
+			title='Another great post',
+			content='This is some sample content',
+			author='Alex')
